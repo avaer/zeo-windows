0785646068ddbd8bb70e9b5a8e3678eaa16aae96
Adrian Biedrzycki <a@modules.io>
2017-12-26 16:43:07 -0800

    Update build

---
+++ index-browser.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const http = require('http');
+const {app, ipcMain, BrowserWindow} = require('electron');
+
+const url = process.argv[2] || ('file://' + path.join(__dirname, 'index.html'));
+
+const _requestAppReady = () => new Promise((accept, reject) => {
+  app.on('ready', () => {
+    accept();
+  });
+  app.on('error', err => {
+    reject(err);
+  });
+});
+
+_requestAppReady()
+  .then(() => {
+    ipcMain.on('ipc', (event, e) => {
+      const {method} = e;
+      switch (method) {
+        case 'show': {
+          win.show();
+          break;
+        }
+        case 'hide': {
+          win.hide();
+          break;
+        }
+        case 'quit': {
+          process.exit(0);
+          break;
+        }
+      }
+    });
+    
+    const win = new BrowserWindow({
+      width: 1280,
+      height: 1024,
+      show: false,
+      backgroundThrottling: false,
+      autoHideMenuBar: true,
+      webPreferences: {
+        preload: path.join(__dirname, 'api.js'),
+        // webSecurity: false,
+      },
+    });
+    win.loadURL(url);
+    win.webContents.openDevTools({
+      mode: 'detach',
+    });
+    win.webContents.on('did-fail-load', () => {
+      process.exit(1);
+    });
+    win.webContents.on('crashed', () => {
+      process.exit(0);
+    });
+    win.webContents.on('devtools-closed', () => {
+      process.exit(0);
+    });
+  })
+  .catch(err => {
+    console.warn(err.stack);
+    process.exit(1);
+  });
