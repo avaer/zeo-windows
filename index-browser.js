a524e6d3003aeda44a4ad47bafbb3690b5dd079c
Adrian Biedrzycki <a@modules.io>
2017-12-26 17:15:17 -0800

    Require url argument in run.cmd

--- index-browser.js
+++ index-browser.js
@@ -2,63 +2,68 @@ const path = require('path');
 const http = require('http');
 const {app, ipcMain, BrowserWindow} = require('electron');
 
-const url = process.argv[2] || ('file://' + path.join(__dirname, 'index.html'));
+const url = process.argv[2];
 
-const _requestAppReady = () => new Promise((accept, reject) => {
-  app.on('ready', () => {
-    accept();
+if (url) {
+  const _requestAppReady = () => new Promise((accept, reject) => {
+    app.on('ready', () => {
+      accept();
+    });
+    app.on('error', err => {
+      reject(err);
+    });
   });
-  app.on('error', err => {
-    reject(err);
-  });
-});
 
-_requestAppReady()
-  .then(() => {
-    ipcMain.on('ipc', (event, e) => {
-      const {method} = e;
-      switch (method) {
-        case 'show': {
-          win.show();
-          break;
+  _requestAppReady()
+    .then(() => {
+      ipcMain.on('ipc', (event, e) => {
+        const {method} = e;
+        switch (method) {
+          case 'show': {
+            win.show();
+            break;
+          }
+          case 'hide': {
+            win.hide();
+            break;
+          }
+          case 'quit': {
+            process.exit(0);
+            break;
+          }
         }
-        case 'hide': {
-          win.hide();
-          break;
-        }
-        case 'quit': {
-          process.exit(0);
-          break;
-        }
-      }
-    });
-    
-    const win = new BrowserWindow({
-      width: 1280,
-      height: 1024,
-      show: false,
-      backgroundThrottling: false,
-      autoHideMenuBar: true,
-      webPreferences: {
-        preload: path.join(__dirname, 'api.js'),
-        // webSecurity: false,
-      },
-    });
-    win.loadURL(url);
-    win.webContents.openDevTools({
-      mode: 'detach',
-    });
-    win.webContents.on('did-fail-load', () => {
+      });
+      
+      const win = new BrowserWindow({
+        width: 1280,
+        height: 1024,
+        show: false,
+        backgroundThrottling: false,
+        autoHideMenuBar: true,
+        webPreferences: {
+          preload: path.join(__dirname, 'api.js'),
+          // webSecurity: false,
+        },
+      });
+      win.loadURL(url);
+      win.webContents.openDevTools({
+        mode: 'detach',
+      });
+      win.webContents.on('did-fail-load', () => {
+        process.exit(1);
+      });
+      win.webContents.on('crashed', () => {
+        process.exit(0);
+      });
+      win.webContents.on('devtools-closed', () => {
+        process.exit(0);
+      });
+    })
+    .catch(err => {
+      console.warn(err.stack);
       process.exit(1);
     });
-    win.webContents.on('crashed', () => {
-      process.exit(0);
-    });
-    win.webContents.on('devtools-closed', () => {
-      process.exit(0);
-    });
-  })
-  .catch(err => {
-    console.warn(err.stack);
-    process.exit(1);
-  });
+} else {
+  console.warn('usage: run.cmd [URL]');
+  process.exit(1);
+}
