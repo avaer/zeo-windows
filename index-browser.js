ed1db3028014a0cbb9d7344f0199d216e9a00e59
Adrian Biedrzycki <a@modules.io>
2017-12-28 11:22:34 -0800

    Break out install to separate lib

--- index-browser.js
+++ index-browser.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 const {http, https} = require('follow-redirects');
-// const http = require('http');
-// const https = require('https');
 const childProcess = require('child_process');
 
 const mkdirp = require('mkdirp');
@@ -10,6 +8,8 @@ const touch = require('touch');
 const tar = require('tar');
 const electron = require('electron');
 const {app, ipcMain, BrowserWindow} = electron;
+
+const installLib = require('./lib/install.js');
 
 const url = process.argv[2];
 
@@ -78,111 +78,22 @@ if (url) {
           }
           case 'createLocalServer': {
             const {args: [id, name]} = e;
-            const dirname = path.join(__dirname, 'servers', name);
 
-            const _downloadRelease = () => new Promise((accept, reject) => {
-              mkdirp(dirname, err => {
-                if (!err) {
-                  const proxyReq = https.get({
-                    host: 'render.zeovr.io',
-                    path: '/release.tar.gz',
-                  }, proxyRes => {
-                    if (proxyRes.statusCode >= 200 && proxyRes.statusCode < 300) {
-                      const contentLength = parseInt(proxyRes.headers['content-length'], 10);
-
-                      const ws = tar.x({
-                        strip: 1,
-                        C: dirname,
-                      });
-                      proxyRes.on('error', reject);
-                      ws.on('error', reject);
-                      proxyRes.pipe(ws);
-
-                      let bytesRead = 0;
-                      proxyRes.on('data', d => {
-                        bytesRead += d.length;
-
-                        win.webContents.send('ipc', {
-                          method: 'progress',
-                          args: [id, bytesRead/contentLength],
-                        });
-                      });
-                      ws.on('finish', () => {
-                        accept();
-                      });
-                    } else {
-                      reject(new Error('download release got invalid status code: ' + proxyRes.statusCode));
-                    }
-                  });
-                  proxyReq.on('error', reject);
-                } else {
-                  reject(err);
-                }
-              });
+            installLib.createLocalServer({
+              name,
             })
-              .then(() => new Promise((accept, reject) => {
-                mkdirp(path.join(dirname, 'data'), err => {
-                  if (!err) {
-                    touch(path.join(dirname, 'data', 'no-hotload.json'), err => {
-                      if (!err) {
-                        accept();
-                      } else {
-                        reject(err);
-                      }
-                    });
-                  } else {
-                    reject(err);
-                  }
+              .then(serverSpec => {
+                win.webContents.send('ipc', {
+                  method: 'response',
+                  args: [id, null, serverSpec],
                 });
-              }));
-            const _startServer = () => {
-              const cp = childProcess.spawn('powershell.exe', ['-NonInteractive', '-Command', `Start-Process -NoNewWindow -RedirectStandardOutput out-log.txt -RedirectStandardError err-log.txt -FilePath ./windows/node/node.exe -ArgumentList "index.js","noTty"`], {
-                cwd: dirname,
-                // detached: true,
-                // stdio: 'ignore'
+              })
+              .catch(err => {
+                win.webContents.send('ipc', {
+                  method: 'response',
+                  args: [id, err.stack, null],
+                });
               });
-              cp.on('error', err => {
-                console.warn(err);
-              });
-              cp.stdout.pipe(process.stdout);
-              cp.stderr.pipe(process.stderr);
-              // cp.unref();
-              return Promise.resolve();
-            };
-
-            fs.lstat(dirname, err => {
-              if (err && err.code === 'ENOENT') {
-                _downloadRelease()
-                  .then(() => _startServer())
-                  .then(() => {
-                    const serverSpec = {
-                      address: '127.0.0.1',
-                      port: 8000,
-                      mods: [],
-                      players: [],
-                      state: {
-                        installing: false,
-                        installed: true,
-                        running: true,
-                      }
-                    }
-                    win.webContents.send('ipc', {
-                      method: 'response',
-                      args: [id, null, serverSpec],
-                    });
-                  })
-                  .catch(err => {
-                    win.webContents.send('ipc', {
-                      method: 'response',
-                      args: [id, err.stack, null],
-                    });
-                  });
-              } else if (!err) {
-                console.warn(new Error('server name already exists:' + name));
-              } else {
-                console.warn(err);
-              }
-            });
             break;
           }
         }
