540326a1ca17488145864cb621887f1f076a03ee
Adrian Biedrzycki <a@modules.io>
2017-12-28 14:45:53 -0800

    Fork external process for create local server install

--- index-browser.js
+++ index-browser.js
@@ -11,148 +11,199 @@ const {app, ipcMain, BrowserWindow} = electron;
 
 const installLib = require('./lib/install.js');
 
-const url = process.argv[2];
+const command = (() => {
+  for (let i = 2; i < process.argv.length; i++) {
+    const arg = process.argv[i];
+    const match = arg.match(/^(install)$/);
+    if (match) {
+      return match[1];
+    }
+  }
+  return null;
+})();
 
-if (url) {
-  app.commandLine.appendSwitch('high-dpi-support', 'true');
-  app.commandLine.appendSwitch('force-device-scale-factor', '1');
+if (command === null) {
+  const url = (() => {
+    for (let i = 2; i < process.argv.length; i++) {
+      const arg = process.argv[i];
+      const match = arg.match(/^url=(.+)$/);
+      if (match) {
+        return match[1];
+      }
+    }
+    return null;
+  })();
+  if (url) {
+    app.commandLine.appendSwitch('high-dpi-support', 'true');
+    app.commandLine.appendSwitch('force-device-scale-factor', '1');
 
-  const _requestAppReady = () => new Promise((accept, reject) => {
-    app.on('ready', () => {
-      accept();
+    const _requestAppReady = () => new Promise((accept, reject) => {
+      app.on('ready', () => {
+        accept();
+      });
+      app.on('error', err => {
+        reject(err);
+      });
     });
-    app.on('error', err => {
-      reject(err);
-    });
-  });
 
-  _requestAppReady()
-    .then(() => {
-      let oldX = 0;
-      let oldY = 0;
-      ipcMain.on('ipc', (event, e) => {
-        const {method} = e;
-        switch (method) {
-          case 'startMove': {
-            const position = win.getPosition();
-            oldX = position[0];
-            oldY = position[1];
-            break;
-          }
-          case 'move': {
-            const {args: [dx, dy]} = e;
-            win.setPosition(oldX + dx, oldY + dy);
-            break;
-          }
-          case 'minimize': {
-            win.minimize();
-            break;
-          }
-          case 'maximize': {
-            win.maximize();
-            break;
-          }
-          case 'unmaximize': {
-            win.unmaximize();
-            break;
-          }
-          case 'restore': {
-            win.restore();
-            break;
-          }
-          case 'close': {
-            win.close();
-            break;
-          }
-          case 'show': {
-            win.show();
-            break;
-          }
-          case 'hide': {
-            win.hide();
-            break;
-          }
-          case 'quit': {
-            process.exit(0);
-            break;
-          }
-          case 'createLocalServer': {
-            const {args: [id, name]} = e;
+    _requestAppReady()
+      .then(() => {
+        let oldX = 0;
+        let oldY = 0;
+        ipcMain.on('ipc', (event, e) => {
+          const {method} = e;
+          switch (method) {
+            case 'startMove': {
+              const position = win.getPosition();
+              oldX = position[0];
+              oldY = position[1];
+              break;
+            }
+            case 'move': {
+              const {args: [dx, dy]} = e;
+              win.setPosition(oldX + dx, oldY + dy);
+              break;
+            }
+            case 'minimize': {
+              win.minimize();
+              break;
+            }
+            case 'maximize': {
+              win.maximize();
+              break;
+            }
+            case 'unmaximize': {
+              win.unmaximize();
+              break;
+            }
+            case 'restore': {
+              win.restore();
+              break;
+            }
+            case 'close': {
+              win.close();
+              break;
+            }
+            case 'show': {
+              win.show();
+              break;
+            }
+            case 'hide': {
+              win.hide();
+              break;
+            }
+            case 'quit': {
+              process.exit(0);
+              break;
+            }
+            case 'createLocalServer': {
+              const {args: [id, name]} = e;
 
-            installLib.createLocalServer({
-              name,
-            })
-              .then(serverSpec => {
-                win.webContents.send('ipc', {
-                  method: 'response',
-                  args: [id, null, serverSpec],
-                });
+              installLib.spawnCreateLocalServer({
+                name,
               })
-              .catch(err => {
-                win.webContents.send('ipc', {
-                  method: 'response',
-                  args: [id, err.stack, null],
+                .then(serverSpec => {
+                  win.webContents.send('ipc', {
+                    method: 'response',
+                    args: [id, null, serverSpec],
+                  });
+                })
+                .catch(err => {
+                  win.webContents.send('ipc', {
+                    method: 'response',
+                    args: [id, err.stack, null],
+                  });
                 });
-              });
-            break;
+              break;
+            }
           }
-        }
-      });
+        });
 
-      const zoomFactor = 1.5;
-      const win = new BrowserWindow({
-        width: 1280 * zoomFactor,
-        height: 1024 * zoomFactor,
-        // show: false,
-        icon: path.join(__dirname, 'icon.png'),
-        frame: false,
-        titleBarStyle: 'hidden',
-        autoHideMenuBar: true,
-        // thickFrame: false,
-        backgroundThrottling: false,
-        // darkTheme: true,
-        webPreferences: {
-          preload: path.join(__dirname, 'api.js'),
-          zoomFactor,
-          // webSecurity: false,
-        },
-      });
-      /* const devtools = new BrowserWindow({
-        webPreferences: {
-          zoomFactor,
-        },
-      }); */
-      win.loadURL(url);
-      // win.webContents.setDevToolsWebContents(devtools.webContents);
-      win.on('maximize', () => {
-        win.webContents.send('ipc', {
-          method: 'maximize',
+        const zoomFactor = 1.5;
+        const win = new BrowserWindow({
+          width: 1280 * zoomFactor,
+          height: 1024 * zoomFactor,
+          // show: false,
+          icon: path.join(__dirname, 'icon.png'),
+          frame: false,
+          titleBarStyle: 'hidden',
+          autoHideMenuBar: true,
+          // thickFrame: false,
+          backgroundThrottling: false,
+          // darkTheme: true,
+          webPreferences: {
+            preload: path.join(__dirname, 'api.js'),
+            zoomFactor,
+            // webSecurity: false,
+          },
         });
-      });
-      win.on('unmaximize', () => {
-        win.webContents.send('ipc', {
-          method: 'unmaximize',
+        /* const devtools = new BrowserWindow({
+          webPreferences: {
+            zoomFactor,
+          },
+        }); */
+        win.loadURL(url);
+        // win.webContents.setDevToolsWebContents(devtools.webContents);
+        win.on('maximize', () => {
+          win.webContents.send('ipc', {
+            method: 'maximize',
+          });
         });
-      });
-      win.webContents.openDevTools({
-        mode: 'bottom',
-        // mode: 'detach',
-      });
-      /*win.webContents.on('did-fail-load', () => {
+        win.on('unmaximize', () => {
+          win.webContents.send('ipc', {
+            method: 'unmaximize',
+          });
+        });
+        win.webContents.openDevTools({
+          mode: 'bottom',
+          // mode: 'detach',
+        });
+        /*win.webContents.on('did-fail-load', () => {
+          process.exit(1);
+        });*/
+        win.webContents.on('crashed', () => {
+          process.exit(0);
+        });
+        win.webContents.on('devtools-closed', () => {
+          process.exit(0);
+        });
+      })
+      .catch(err => {
+        console.warn(err.stack);
         process.exit(1);
-      });*/
-      win.webContents.on('crashed', () => {
-        process.exit(0);
       });
-      win.webContents.on('devtools-closed', () => {
-        process.exit(0);
-      });
+  } else {
+    console.warn('usage: run.cmd [URL]');
+    process.exit(1);
+  }
+} else if (command === 'install') {
+  const name = (() => {
+    for (let i = 2; i < process.argv.length; i++) {
+      const arg = process.argv[i];
+      const match = arg.match(/^name=(.+)$/);
+      if (match) {
+        return match[1];
+      }
+    }
+    return null;
+  })();
+  if (name) {
+    installLib.createLocalServer({
+      name,
     })
-    .catch(err => {
-      console.warn(err.stack);
-      process.exit(1);
-    });
+      .then(serverSpec => {
+        console.log(JSON.stringify(serverSpec));
+        process.exit();
+      })
+      .catch(err => {
+        console.log(JSON.stringify({error: err.stack}));
+        process.exit(1);
+      });
+  } else {
+    console.warn('usage: run.cmd [URL]');
+    process.exit(1);
+  }
 } else {
-  console.warn('usage: run.cmd [URL]');
+  console.warn('unknown command: ' + command);
   process.exit(1);
 }
